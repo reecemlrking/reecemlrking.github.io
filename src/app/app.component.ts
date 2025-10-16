125d70798c4d09d8b480837697a9c229321ffb70
Reece King <reece@ualberta.ca>
2025-10-16 11:40:04 -0600

    created header and started bio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { BioComponent } from "./bio/bio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, BioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
