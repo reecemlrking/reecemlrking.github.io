4327118f82b39d86a5f63ac7d4e0cdad1c503ac4
Reece King <reece@ualberta.ca>
2025-12-17 15:47:36 -0700

    modified styles and added Languages compontent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { BioComponent } from "./bio/bio.component";
+import { LanguagesComponent } from './languages/languages.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, BioComponent],
+  imports: [RouterOutlet, HeaderComponent, BioComponent, LanguagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/languages/languages.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {
+  AccordianButtonDirective,
+  AccordianComponent,
+  AccordianItemComponent,
+  TemplateIdDirective
+} from '@angular/aria';
+
+@Component({
+  selector: 'app-languages',
+  imports: [],
+  templateUrl: './languages.component.html',
+  styleUrl: './languages.component.css'
+})
+
+
+export class LanguagesComponent {
+
+}
+
+
+
+
