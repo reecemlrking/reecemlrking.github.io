e49f21e5d04834cc22a83b9d30258599f8fa76a2
Reece King <reeceking9922@gmail.com>
2025-12-18 11:18:03 -0700

    added programming languages and icons

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css', '../app.component.css']
 })
 export class HeaderComponent {
 
--- src/app/languages/languages.component.ts
+++ src/app/languages/languages.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import {
-  AccordionGroup,
-  AccordionTrigger,
-  AccordionPanel,
-  AccordionContent,
-} from '@angular/aria/accordion';
 
 @Component({
   selector: 'app-languages',
-  imports: [AccordionGroup, AccordionTrigger, AccordionPanel, AccordionContent],
+  imports: [],
   templateUrl: './languages.component.html',
   styleUrl: './languages.component.css',
 })
