af9285915e5a2ad85c4b58efebc66707cd11a804
Reece King <reece@ualberta.ca>
2025-12-18 08:26:40 -0700

    regenerated languages component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { BioComponent } from "./bio/bio.component";
-import { LanguagesComponent } from './languages/languages.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, BioComponent, LanguagesComponent],
+  imports: [RouterOutlet, HeaderComponent, BioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/languages/languages.component.ts
+++ src/app/languages/languages.component.ts
@@ -1,23 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  AccordianButtonDirective,
-  AccordianComponent,
-  AccordianItemComponent,
-  TemplateIdDirective
-} from '@angular/aria';
 
 @Component({
   selector: 'app-languages',
   imports: [],
   templateUrl: './languages.component.html',
-  styleUrl: './languages.component.css'
+  styleUrl: './languages.component.css',
 })
-
-
 export class LanguagesComponent {
 
 }
-
-
-
-
